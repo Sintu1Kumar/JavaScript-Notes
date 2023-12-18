4c6723231401fec174a21ead87b791ac9e024ad6
Sintu Kumar <kumarsintu499@gmail.com>
2023-12-18 19:04:19 +0530

    add assignment 4

---
+++ Assignments/Assignment-4.js
@@ -0,0 +1,35 @@
+// Day4 JavaScript Questions:-
+
+// 1. Create the Example for Logical Operators?
+// Ans:-  Three type of Logical Operators.
+//     1. And Operator(&&)
+//     2. Or Operator(||)
+//     3. Not Operator(~)
+
+// Ex- let a = 30;
+//     console.log("Number is divisible by both (3 and 5) : ", a%3==0 && a%5==0);
+
+// 2. Difference between “ == “ and “ === “ operators.
+// Ans:-"==" it is only compare the two value.
+//      "===" it is compare the two value and also check its data type.
+
+// 3. How many operators do we have in JS?
+// Ans:- There are various types of JavaScript operators.
+//      Arithmetic Operators
+//      Assignment Operators
+//      Comparison Operators
+//      Logical Operators
+//      Bitwise Operators
+//      Ternary Operators
+//      Comma Operators
+//      Unary Operators
+//      Relational Operators
+//      BigInt Operators
+//      String Operators
+
+// 4. What is use of parseInt in js?
+// Ans:- It is converted to the value in number or Integer.
+
+// 5. Take input from user and find the odd and even number.
+// Ans:- let a = Number(prompt("Enter the number"));
+//       console.log((a%2==0) ? "Number is Even" : "Number is odd");
--- script .js
+++ script .js
@@ -1,1 +1,21 @@
-console.log("i am Sintu Kumar");
+// console.log("i am Sintu Kumar");
+// let a = 12;
+// console.log(a)
+
+// let name = prompt("enter your name");
+// console.log(name)
+
+// let a = 10;
+// let b = 90;
+// let c = 100;
+
+// let result = a<b && a<c;
+// console.log("is a the less among three of the number : "+result)
+
+let a = Number(prompt("Enter the number"));
+
+// console.log("Enter number is odd :",a%2!=0);
+
+// console.log("Number is divisible by both(3 and 5) : ", a%3==0 && a%5==0);
+
+console.log((a%2==0) ? "Number is Even" : "Number is odd");
