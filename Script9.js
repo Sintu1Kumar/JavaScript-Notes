c48ad7acc3975b8f5f75575c14baea086e703241
Sintu Kumar <kumarsintu499@gmail.com>
2024-01-05 23:26:50 +0530

    add

--- Script9.js
+++ Script9.js
@@ -1,22 +1,46 @@
 let text = document.getElementById("demo");
 
-let arr = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr[i].length; j++) {
+//     console.log(arr[i][j]);
+//   }
+// }
+
+// let num = "";
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr[i].length; j++) {
+//     num += arr[i][j] + "..";
+//   }
+//   num += "\n";
+// }
+// text.innerHTML = num;
+
+
+
+// Q5. Write a JS code to print a 2D array
+let arr4 = [
+  [1, 2, 6],
+  [3, 4, 8],
+  [5, 6, 9],
 ];
 
-for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr[i].length; j++) {
-    console.log(arr[i][j]);
+let num = "[";
+for (let i = 0; i < arr4.length; i++) {
+  (i < 1) ? (num += "[ ") : (num += " [ ");
+  for (let j = 0; j < arr4[i].length; j++) {
+    num += arr4[i][j] + " ";
+  }
+  if (i != arr4.length - 1) {
+    num += "]" + "\n";
+  } else {
+    num += "]]";
   }
 }
-
-let num = "";
-for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr[i].length; j++) {
-    num += arr[i][j] + "..";
-  }
-  num += "\n";
-}
-text.innerHTML = num;
+text.innerHTML = (num);
+console.log(num)
