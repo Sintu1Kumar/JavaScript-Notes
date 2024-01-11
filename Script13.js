d4be2be51b7869a52a2cc5ae8024bbd1686f4837
Sintu Kumar <kumarsintu499@gmail.com>
2024-01-11 14:57:25 +0530

    add script 13

---
+++ Script13.js
@@ -0,0 +1,11 @@
+//* function exepresion:-
+
+let print = function(){
+    console.log("Hello world")
+}
+print();
+
+let greeting = function abc(){
+    console.log("Happy Birthday")
+}
+greeting();
