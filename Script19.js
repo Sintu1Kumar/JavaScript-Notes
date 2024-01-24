1b17d258624e52ebba373b200e05814febc4b788
Sintu Kumar <kumarsintu499@gmail.com>
2024-01-24 13:07:00 +0530

    add script 19

---
+++ Script19.js
@@ -0,0 +1,43 @@
+console.log("Welcome to Script 19");
+
+// arr.concat
+let arr = [1, 2, 3, 4, 5];
+let arr2 = ["sintu", "mohan"]
+let arr3 = arr.concat(arr2);
+console.log(arr3)
+
+// arr.every 
+// arr.fill
+// arr.map
+// arr.find
+// arr.findIndex
+// arr.includes
+// arr.toString
+// arr.indexOf
+// arr.reverse
+// arr.sort
+// arr.join
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let obj = {
+//   0: 1,
+//   1: 2,
+//   2: 32,
+//   3: 4,
+//   4: 56,
+//   5: 34,
+//   length: 6,
+
+//   forEach: function (fn) {
+//     for (let i = 0; i < this.length; i++) {
+//       let ele = this[i];
+//       fn(ele, i);
+//     }
+//   },
+// };
+
+// obj.forEach(function (ele, i) {
+//   console.log(ele + " == " + i);
+// });
