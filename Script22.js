86f0592259a4f0bbdcf8c84e48ed391ac9517c3b
Sintu Kumar <kumarsintu499@gmail.com>
2024-02-05 14:23:25 +0530

    add async

---
+++ Script22.js
@@ -0,0 +1,47 @@
+// 1. Write a JavaScript function to calculate the sum of two numbers.
+// let num1 = 20;
+// let num2 = 30;
+// let result = 0;
+// function add(){
+//     console.log(num1 + num2);
+// };
+// add();
+
+// 2. Write a JavaScript program to find the maximum number in an array.
+// let arr = [1,5,8,96,3,7,8]
+// let result = 0;
+// let max = arr[0];
+// for(let i = 1; i<arr.length; i++){
+//     if(arr[i]>max){
+//         result = arr[i];
+//     }
+//     return max;
+// }
+// console.log(result);
+  
+// 3. Write a JavaScript function to check if a given string is a palindrome (reads the same forwards and backwards). 
+// 4. Write a JavaScript program to reverse a given string.
+// let str = "Sintu kumar";
+// let strResult = str.split("").reverse().join("");
+// console.log(strResult);
+
+// 5. Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers.
+// let arr1 = [1,5,8,96,32,7,8]
+//  let result1 = arr1.map((e)=>{
+//     if(e%2===0){
+//         reslut2 = arr1[e]
+//     }
+// })
+// console.log(result2);
+
+// 6. Write a JavaScript program to calculate the factorial of a given number.
+let num3 = 5;
+let result2 = 0;
+for(let i = 0; i<num3; i++){
+    result2 += reslut2 * i;
+} 
+console.log(reslut2);
+// 7. Write a JavaScript function to check if a given number is prime. 
+// 8. Write a JavaScript program to find the largest element in a nested array. 
+// 9. Write a JavaScript function that returns the Fibonacci sequence up to a given number of terms. 
+// 10. Write a JavaScript program to convert a string to title case (capitalize the first letter of each word).
