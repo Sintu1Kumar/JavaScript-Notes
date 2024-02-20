6750cc2162dce5743315420d49d334d88536cfe2
Sintu Kumar <kumarsintu499@gmail.com>
2024-02-20 19:32:42 +0530

    add script 27

---
+++ Script27.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function () {
+  console.log("page has been loaded");
+
+  let buttonvar = this.document.getElementById("buttonpress");
+  let para = this.document.getElementById("para");
+
+  buttonvar.addEventListener("click", function () {
+    para.innerText = "the button has been clicked";
+  });
+  setTimeout(() => {
+    buttonvar.style.width = "500px";
+    buttonvar.style.backgroundColor = "yellow";
+  }, 5000);
+
+  console.log(buttonvar);
+});
