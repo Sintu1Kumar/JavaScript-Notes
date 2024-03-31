5af56812f0bea7af441f1298428fdb84c5970933
Sintu Kumar <kumarsintu499@gmail.com>
2024-03-31 23:44:05 +0530

    add pattern example

--- Script9.js
+++ Script9.js
@@ -1,25 +1,25 @@
 let text = document.getElementById("demo");
 
-// let arr = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-//   [7, 8, 9],
-// ];
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
 
-// for (let i = 0; i < arr.length; i++) {
-//   for (let j = 0; j < arr[i].length; j++) {
-//     console.log(arr[i][j]);
-//   }
-// }
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
 
-// let num = "";
-// for (let i = 0; i < arr.length; i++) {
-//   for (let j = 0; j < arr[i].length; j++) {
-//     num += arr[i][j] + "..";
-//   }
-//   num += "\n";
-// }
-// text.innerHTML = num;
+let num = "";
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    num += arr[i][j] + "..";
+  }
+  num += "\n";
+}
+text.innerHTML = num;
 
 
 
@@ -30,17 +30,17 @@ let arr4 = [
   [5, 6, 9],
 ];
 
-let num = "[";
+let num1 = "[";
 for (let i = 0; i < arr4.length; i++) {
-  (i < 1) ? (num += "[ ") : (num += " [ ");
+  (i < 1) ? (num1 += "[ ") : (num1 += " [ ");
   for (let j = 0; j < arr4[i].length; j++) {
-    num += arr4[i][j] + " ";
+    num1 += arr4[i][j] + " ";
   }
   if (i != arr4.length - 1) {
-    num += "]" + "\n";
+    num1 += "]" + "\n";
   } else {
-    num += "]]";
+    num1 += "]]";
   }
 }
-text.innerHTML = (num);
-console.log(num)
+text.innerHTML = (num1);
+console.log(num1)
